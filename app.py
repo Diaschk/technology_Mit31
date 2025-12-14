0fd4d3ea3a96602d0200d9c358c773936ad7275c
Diaschk <yovhymyschdiana@gmail.com>
2025-12-14 11:34:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from app import Flask
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -10,3 +10,4 @@ def home():
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8083)
     
+
